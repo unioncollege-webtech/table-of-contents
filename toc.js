2ee91019347867a3b598d5434e110f6a99ec1b70
mrdcs92 <mrdcs92@gmail.com>
2015-10-28 20:42:47 +0000

    appended ul list to inside li

--- toc.js
+++ toc.js
@@ -1,41 +1,43 @@
-//
-// 1) Auto-generate a Table of Contents
-//
+// toc.js
 
-// Use document.querySelector all to find all the headings in the article 
-// **except** for the h1 (i.e. h2, h3, h4, h5, h6)
 
 var headings = document.querySelectorAll("h2, h3, h4, h5, h6");
 
-// Sanity check: console.log the 
-//console.log(headings);
 
-// Use document.createElement to create a new `ol` element that will hold the
-// table of contents list
 var tocList = document.createElement('ol');
 
-// Sanity check: console.log the `ol` element
-//console.log(tocList);
 
 for (var i = 0; i < headings.length; i++) {
     var heading = headings[i];
     
     var ul = document.createElement('ul');
+    
+    if (headings[i + 1] === undefined) {
+        break;
+    }
 
-    if (heading.tagName === 'H3' && headings[i - 1].tagName === 'H2') {
-        var h = i;
+    if (heading.tagName === 'H2' && headings[i + 1].tagName === 'H3') {
+        var h = i + 1;
+        var a = document.createElement('a');
+            a.href = "#" + headings[i].id;
+            a.rel = "internal";
+            a.textContent = headings[i].textContent;
+        var li = document.createElement('li');
+        li.appendChild(a);
+        
         while (headings[h].tagName === 'H3') {
             var a = document.createElement('a');
             a.href = "#" + headings[h].id;
             a.rel = "internal";
             a.textContent = headings[h].textContent;
-            var li = document.createElement('li');
-            li.appendChild(a);
-            ul.appendChild(li);
+            var li2 = document.createElement('li');
+            li2.appendChild(a);
+            ul.appendChild(li2);
             h++;
         }
-        tocList.appendChild(ul);
-        i = h - 1;
+        li.appendChild(ul);
+        tocList.appendChild(li);
+        i = h;
     } else {
         var a = document.createElement('a');
         a.href = "#" + heading.id;
@@ -48,114 +50,18 @@ for (var i = 0; i < headings.length; i++) {
     }
 }
 
-
-//console.log(tocList);
-
-// Loop through the headings using a 'for' loop, and create an ordered list of 
-// links to the different sections of the document. The final list of
-// links will look like the following:
-//
-// <ol>
-//   <li>
-//     <a href="#description" rel="internal">Description</a>
-//   </li>
-//   <li>
-//     <a href="#create-zoo-js-with-the-animals-array" rel="internal">Create zoo.js with the animals array</a>
-//   </li>
-//   <li>
-//     <a href="#sort-and-display-the-animal-gallery" rel="internal">Sort and display the animal gallery</a>
-//   </li>
-//   <li>
-//     <a href="#display-the-featured-animal" rel="internal">Display the featured animal</a>
-//   </li>
-//   <li>
-//     <a href="#map-sort-and-display-animal-ages" rel="internal">Map, sort, and display animal ages</a>
-//   </li>
-//   <li>
-//     <a href="#animals" rel="internal">Animals</a>
-//   </li>
-//   ...
-// </ol>
-
-// Sanity check: console.log the `ol` containing the table of contents
-
-// Use document.querySelector to find the <aside> element with the id
-// "table-of-contents". Store this in a variable called `toc`.
 var toc = document.querySelector("#table-of-contents");
 
-// Sanity check: console.log the `toc` variable
-//console.log(toc);
-
-// Append the `ol` containing the table of contents list to `toc`.
 toc.appendChild(tocList);
 
-// Use document.createElement to generate an h2 element with the text 
-// "Table of Contents"
 var tocHeading = document.createElement('h2');
 tocHeading.textContent = 'Table of Contents';
-//console.log(tocHeading);
 
-// Use insertBefore to add the heading to `toc` *before* the `ol` containing the
 toc.insertBefore(tocHeading, tocList);
-// list of internal links.
 
-// Sanity check: console.log `toc`
-//console.log(toc);
-
-
-
-//
-// 2) Make it collapsible
-//
-
-// use addEventListener to listen for 'click' events on the "Table of Contents"
-// h2 element.
 tocHeading.addEventListener('click', function() {
-    //console.log("Clicked!");
     tocList.classList.toggle('collapsed');
     console.log(tocList);
 }, false);
 
-// When the Table of Contents heading is clicked, use .classList.toggle() to add
-// or remove the 'collapsed' class name on the `ol` list of links.
 
-// Add a style.css file with a rule to hide (using `display: none;`) elements
-// that have the `collapsed` class name.
-
-
-
-
-
-
-//
-// 3) Make the TOC a nested list: (h3s are nested inside h2s) 
-/*
-DO THIS STEP NEXT, 10/22/15
-*/
-//
-
-// Update the code generating the table of contents list above to make it a 
-// nested list like so:
-//
-// <ol>
-//   <li>
-//     <a href="#description" rel="internal">Description</a>
-//     <ol>
-//       <li>
-//         <a href="#create-zoo-js-with-the-animals-array" rel="internal">Create zoo.js with the animals array</a>
-//       </li>
-//       <li>
-//         <a href="#sort-and-display-the-animal-gallery" rel="internal">Sort and display the animal gallery</a>
-//       </li>
-//       <li>
-//         <a href="#display-the-featured-animal" rel="internal">Display the featured animal</a>
-//       </li>
-//       <li>
-//         <a href="#map-sort-and-display-animal-ages" rel="internal">Map, sort, and display animal ages</a>
-//       </li>
-//     </ol>
-//   <li>
-//     <a href="#animals" rel="internal">Animals</a>
-//   </li>
-//   ...
-// </ol>
