950ce3e2b58a0f17b4b6abd1a3f2a0b006a768e1
Caleb <cawright@ucollege.edu>
2015-11-03 02:48:33 +0000

    finished main part

--- toc.js
+++ toc.js
@@ -4,14 +4,14 @@
 
 // Use document.querySelector all to find all the headings in the article 
 // **except** for the h1 (i.e. h2, h3, h4, h5, h6)
-
+var headings = document.querySelectorAll("h2, h3, h4, h5, h6");
 // Sanity check: console.log the headings
-
+// console.log(headings);
 // Use document.createElement to create a new `ol` element that will hold the
 // table of contents list
-
+var tocList = document.createElement('ol');
 // Sanity check: console.log the `ol` element
-
+console.log(tocList);
 // Loop through the headings using a 'for' loop, and create an ordered list of 
 // links to the different sections of the document. The final list of
 // links will look like the following:
@@ -37,24 +37,49 @@
 //   </li>
 //   ...
 // </ol>
+for(var i=0; i < headings.length; i++){
+    var heading = headings[i];
+    
+    // console.log(heading.id);
+    // console.log(heading.textContent);
+    
+    var a = document.createElement('a');
+    a.href = "#" + heading.id;
+    a.rel = 'internal';
+    a.textContent = heading.textContent;
+    // console.log(a);
+    
+    var li = document.createElement('li');
+    li.appendChild(a);
+    
+    tocList.appendChild(li);
+}
 
 // Sanity check: console.log the `ol` containing the table of contents
+console.log(tocList);
 
 // Use document.querySelector to find the <aside> element with the id
 // "table-of-contents". Store this in a variable called `toc`.
+var toc = document.querySelector('#table-of-contents');
 
 // Sanity check: console.log the `toc` variable
+console.log(toc);
 
 // Append the `ol` containing the table of contents list to `toc`.
+toc.appendChild(tocList);
 
 // Use document.createElement to generate an h2 element with the text 
 // "Table of Contents"
+var tocHeading = document.createElement('h2');
+tocHeading.textContent = 'Table of Contents';
+// console.log(tocHeading);
 
 // Use insertBefore to add the heading to `toc` *before* the `ol` containing the
 // list of internal links.
+toc.insertBefore(tocHeading, tocList);
 
 // Sanity check: console.log `toc`
-
+console.log(toc);
 
 
 //
@@ -63,7 +88,11 @@
 
 // use addEventListener to listen for 'click' events on the "Table of Contents"
 // h2 element.
-
+tocHeading.addEventListener('click', function(){
+    // console.log('Clicked!')
+    tocList.classList.toggle('collapsed');
+    console.log(tocList);
+}, false)
 // When the Table of Contents heading is clicked, use .classList.toggle() to add
 // or remove the 'collapsed' class name on the `ol` list of links.
 
