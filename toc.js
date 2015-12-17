31dd7c855c0d63a045b684e095700d0f532fc690
Thomas Birai <ucollege23@gmail.com>
2015-12-17 06:29:43 +0000

    completed table-of-contents

--- toc.js
+++ toc.js
@@ -1,28 +1,45 @@
-// Find the heading elements
-
-
-// Create an ordered list
-
-
-// Loop through the headings
-
-
-// Build the `a` and `li` elements
-
-
-// Find the Table of Contents placeholder element
-
-
-// Add the `tocList` to the `toc` placeholder element
-
-
-// Add a “Table of Contents” heading
-
-
-// Make it collapsible
-
-
-// Celebrate!
-
-
-// Additional improvements
+function autoTableOfContents() {
+    var headings = document.querySelectorAll("h2, h3, h4, h5, h6");
+    var tocList = document.createElement('ol');
+    
+    var currentList = tocList;
+    var previousHeadingSize = 0;
+    var previousLi;
+    for(var i = 0; i < headings.length; i++) {
+        var heading = headings[i];
+        
+        var headingSize = parseInt(heading.nodeName.replace(/^\D+/g, ''));
+        if (previousHeadingSize != 0 && previousHeadingSize < headingSize) {
+            var newOl = document.createElement('ol');
+            previousLi.appendChild(newOl);
+            currentList = newOl;
+        }
+        
+        if (previousHeadingSize > headingSize) {
+            currentList = tocList;
+        }
+        
+        var li = document.createElement('li');
+        var a = document.createElement('a');
+        a.href = '#' + heading.id;
+        a.textContent = heading.textContent;
+        a.rel = 'internal';
+    
+        li.appendChild(a);
+        currentList.appendChild(li);
+        
+        previousLi = li;
+        previousHeadingSize = headingSize;
+    }
+    
+    var toc = document.querySelector('#table-of-contents');
+    toc.appendChild(tocList);
+    
+    var tocHeading = document.createElement('h2');
+    tocHeading.textContent = 'Table of Contents';
+    toc.insertBefore(tocHeading, tocList);
+    
+    tocHeading.addEventListener('click', function(){
+        tocList.classList.toggle('collapsed');
+    }, false);
+}
